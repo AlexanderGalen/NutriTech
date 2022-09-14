e2eafac40f19ba31018aa276f05cb46198d519ef
Ruben <rubendc91@gmail.com>
2022-09-14 12:46:42 -0700

    pseudo Code

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//get user input
+
+//get info from spoonacular api
+
+//use spoonacular api data to search meal info
+
+//use spoonacular api data to call charts api
+
+//populate rsponsefor charts and spoonacular on html
+
+// populate recent search
+
+//save recent searches to local storage
+
